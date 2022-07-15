6c2de928c6dfc2e18a813ddc2ba5713316eefb58
BChip <chippi.bradley@gmail.com>
2022-07-14 23:22:34 -0400

    more things off

--- extension/.eslintrc.js
+++ extension/.eslintrc.js
@@ -29,6 +29,10 @@ module.exports = {
         // suppress errors for missing 'import React' in files
         "react/react-in-jsx-scope": "off",
         // allow jsx syntax in js files (for next.js project)
-        "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }], //should add ".ts" if typescript project
+        "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }], //should add ".ts" if typescript project,
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/no-explicit-any": "off"
     }
 }
